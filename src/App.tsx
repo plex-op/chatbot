5c8771331868dacf219910006044d1d457d90948
plex-op <karthikkn877@gmail.com>
2026-04-23 11:57:43 +0530

    fix main

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        background: "#000",
+        color: "#fff",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "20px",
+      }}
+    >
+      HAKA.media Chatbot is Live 🚀
+    </div>
+  );
+}
