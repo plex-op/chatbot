2d7dd2f7c70b9f24ebdb09d95f21f20112e39d27
plex-op <karthikkn877@gmail.com>
2026-04-23 12:03:07 +0530

    fix app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
+import { HakaConsultation } from "./components/HakaConsultation";
+
 export default function App() {
-  return (
-    <div
-      style={{
-        background: "#000",
-        color: "#fff",
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        fontSize: "20px",
-      }}
-    >
-      HAKA.media Chatbot is Live 🚀
-    </div>
-  );
+  return <HakaConsultation />;
 }
